b8c7a6b84938a6045c6066c2e6ad34008157ea97
Nathan Gilbert <nathan.gilbert@gmail.com>
2018-02-03 15:45:20 -0700

    adding sqlite db

--- tweet_manager.py
+++ tweet_manager.py
@@ -3,6 +3,7 @@
 """ This module will take in a list of tweets that need to be retweeted """
 
 import random
+import sqlite3
 from datetime import datetime
 
 class TweetManager:
@@ -12,6 +13,34 @@ class TweetManager:
         self._focus_tweet_list = []
         # these are the most recent tweets and should be retweeted randomly
         self._most_recent_tweet_list = []
+        self.sqlite_name = "tweet_list.sqlite"
+
+    def init_sqlite_db(self):
+        conn = sqlite3.connect(self.sqlite_name)
+        c = conn.cursor()
+        c.execute('''CREATE TABLE tweet_list (id integer PRIMARY KEY AUTOINCREMENT, msg text, tweet_count integer,
+                  last_tweeted text, stop_date text)''')
+        conn.commit()
+        conn.close()
+
+    def insert_sqlite_tweet(self, msg, stop_date=""):
+        conn = sqlite3.connect(self.sqlite_name)
+        c = conn.cursor()
+        c.execute('''INSERT INTO tweet_list(msg, tweet_count, last_tweeted, stop_date)
+                  VALUES ('{0}', 0, '', '{1}')'''.format(msg, stop_date))
+        conn.commit()
+        conn.close()
+
+    def rando_sqlite_tweet(self):
+        #TODO update last tweeted date & count
+        pass
+
+    def delete_sqlite_tweet(self, m):
+        pass
+
+    def cleanup_sqlite_tweets(self):
+        """ delete all tweets that are beyond the stop_data """
+        pass
 
     def add_focus_tweet(self, tweet):
         """ add a tweet to the focus list """
@@ -36,3 +65,8 @@ class TweetManager:
     def is_last_50_tweet(self, new_tweet):
         """Is the 'new_tweet' one of the last 50 tweets in the TL"""
         return bool(self._most_recent_tweet_list[-50:].count(new_tweet) > 0)
+
+if __name__ == "__main__":
+    tm = TweetManager()
+    #tm.init_sqlite_db()
+    #tm.insert_sqlite_tweet("yellow", "2019")
